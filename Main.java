9417a69e1bbe164345df7a6151dfbf8a87a16987
shwetan03 <43916633+shwetan03@users.noreply.github.com>
2022-01-13 13:16:51 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,33 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        StringTree morseCode = createMorseTree();
+        userPrompt();
+    }
+
+    public static void userPrompt() {
+        System.out.println("Would you like to decode or encode text? Type 'd' to decode and 'e' to encode");
+        System.out.print("> ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        do {
+           if (input.equals("d")) {
+               System.out.println("Please type the phrase you would like decoded");
+               System.out.print("> ");
+               sc.nextLine();
+               input = sc.nextLine();
+               StringTree morseCode = createMorseTree();
+               System.out.println("Your phrase decoded is: " + decode(morseCode, input));
+               System.out.println("Would you like to decode or encode text? Type 'd' to decode and 'e' to encode. Type 'q' to quit");
+               input = sc.next();               ;
+           } else if (input.equals("e")) {
+               System.out.println("Please type the phrase you would like encoded");
+               System.out.print("> ");
+               input = sc.nextLine();
+               StringTree morseCode = createMorseTree();
+               System.out.println("Would you like to decode or encode text? Type 'd' to decode and 'e' to encode. Type 'q' to quit");
+               input = sc.next();
+           }
+        } while (!input.equals("q"));
     }
 
     public static StringTree createMorseTree() {
@@ -32,6 +58,7 @@ public class Main {
         morseCode.getOverallRoot().left.right.left.left = new StringTreeNode("l");
         morseCode.getOverallRoot().left.right.right.left = new StringTreeNode("p");
         morseCode.getOverallRoot().left.right.right.right = new StringTreeNode("j");
+        morseCode.getOverallRoot().left.left.right.right = new StringTreeNode("-");
 
         morseCode.getOverallRoot().right.left.left.left = new StringTreeNode("b");
         morseCode.getOverallRoot().right.left.left.right = new StringTreeNode("x");
@@ -39,6 +66,8 @@ public class Main {
         morseCode.getOverallRoot().right.left.right.right = new StringTreeNode("y");
         morseCode.getOverallRoot().right.right.left.left = new StringTreeNode("z");
         morseCode.getOverallRoot().right.right.left.right = new StringTreeNode("q");
+        morseCode.getOverallRoot().right.right.right.left = new StringTreeNode("-");
+        morseCode.getOverallRoot().right.right.right.right = new StringTreeNode("-");
 
         morseCode.getOverallRoot().left.right.right.right.right = new StringTreeNode("1");
         morseCode.getOverallRoot().left.left.right.right.right = new StringTreeNode("2");
@@ -56,10 +85,10 @@ public class Main {
 
     public static void encode(String text) {
         // Input letter  output morse
-        
+        //
     }
 
-    public static void decode(StringTree tree, String text) {
+    public static String decode(StringTree tree, String text) {
         // input morse output letter
         Queue<String> morse = new LinkedList<>();
         Scanner sc = new Scanner(text);
@@ -68,10 +97,28 @@ public class Main {
         }
 
         StringBuilder decoded = new StringBuilder();
+        while (!morse.isEmpty()) {
+            decoded.append(traverseTree(tree.getOverallRoot(), morse.remove()));
+        }
+
+        String decodedString = new String(decoded);
+        return decodedString;
 
     }
 
-    public static void traverseTree(StringTree tree, String morseChar) {
-
+    public static String traverseTree(StringTreeNode tree, String morseChar) {
+        if (morseChar.length() == 1) {
+            if (morseChar.equals(".")) {
+                return tree.left.data;
+            } else {
+                return tree.right.data;
+            }
+        } else {
+            if (morseChar.substring(0, 1).equals(".")) {
+                return traverseTree(tree.left, morseChar.substring(1));
+            } else {
+                return traverseTree(tree.right, morseChar.substring(1));
+            }
+        }
     }
 }
